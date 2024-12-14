3f3ac59bf33bb8b5539aacd552d209a8cf4f59b9
syu-y <syu@12580526@gmail.com>
2024-12-15 01:25:51 +0900

    テーマ切り替えスイッチ追加

--- src/components/app/AppHeader.vue
+++ src/components/app/AppHeader.vue
@@ -3,21 +3,41 @@
     color="primary" 
     :elevation="2" 
     elevate-on-scroll>
+
     <v-app-bar-nav-icon @click="isOpen = !isOpen"/>
+
     <NuxtLink to="/" class="app-title">
       <v-app-bar-title>
         Application Bar
       </v-app-bar-title>
     </NuxtLink>
+    <template v-slot:append>
+      <v-switch
+          v-model="darkTheme"
+          @update:model-value="changeTheme"
+          :prepend-icon="darkTheme ? 'mdi-weather-night' : 'mdi-weather-sunny'"
+          hide-details
+          inset
+          />
+    </template>
+    <AppNavigation :is-open="isOpen"/>
   </v-app-bar>
-  <AppNavigation :is-open="isOpen"/>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+import { useTheme } from 'vuetify'
+
 // ナビゲーションの表示・非表示を制御
 const isOpen = useState('isOpen', () => false)
-</script>
 
+const darkTheme = ref(false)
+const theme = useTheme()
+
+const changeTheme = () => {
+  theme.global.name.value = darkTheme.value ? 'dark' : 'light'
+}
+</script>
 
 <style scoped>
 .app-title{
